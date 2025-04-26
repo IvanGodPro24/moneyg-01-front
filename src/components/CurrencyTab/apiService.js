dea20121528584e1ce9bd6237f3e0a66b727ddc5
dfostakovskyi <d.fostakovskiy@gmail.com>
2025-04-25 19:01:29 -0700

    Додав запит курсу валют з відображенням результату у таблиці. Стилі не додавав бо неможливо їх протестувати без сторінки

---
+++ src/components/CurrencyTab/apiService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://api.monobank.ua/bank/currency";
+
+export const fetchExchangeRates = async () => {
+  try {
+    const response = await axios.get(API_BASE_URL);
+    return response.data; // Дані про валютні курси
+  } catch (error) {
+    console.error("Error fetching exchange rates:", error);
+    throw error; // Можна обробляти помилку у викликах функції
+  }
+};

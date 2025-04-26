0a22438b81cd590e1842ea6aa4a517f078124f9e
IvanGodPro24 <vanyanep@gmail.com>
2025-04-26 13:38:25 +0300

    Update and fix structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,56 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import DashboardPage from "./components/DashboardPage/DashboardPage";
+import { Routes, Route, Navigate } from "react-router-dom";
 import RestrictedRoute from "./RestrictedRoute";
 import PrivateRoute from "./PrivateRoute";
+import Loader from "./components/Loader/Loader";
+import { lazy, Suspense, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { current } from "./redux/auth/operations";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import { Toaster } from "sonner";
+
+const RegistrationPage = lazy(() =>
+  import("./pages/RegistrationPage/RegistrationPage")
+);
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const DashboardPage = lazy(() =>
+  import("./components/DashboardPage/DashboardPage")
+);
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/register" element={<RegistrationPage />} />
-      <Route
-        path="/"
-        element={
-          <RestrictedRoute component={<LoginPage />} redirectTo="/dashboard" />
-        }
-      />
-      <Route
-        path="/dashboard"
-        element={<PrivateRoute component={<DashboardPage />} />}
-      ></Route>
-    </Routes>
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(current());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <>
+      <Toaster expand position="top-center"></Toaster>
+
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route
+            path="/"
+            element={
+              <RestrictedRoute
+                component={<LoginPage />}
+                redirectTo="/dashboard"
+              />
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={<PrivateRoute component={<DashboardPage />} />}
+          ></Route>
+          <Route path="*" element={<Navigate to="/" replace />}></Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 

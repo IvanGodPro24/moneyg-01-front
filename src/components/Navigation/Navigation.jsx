be3c8681da3be912393c03a0b01929130d843581
Dmytro Koval <kdmytro793@gmail.com>
2025-04-29 21:13:03 +0300

    fix-link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,25 +4,30 @@ import { FaDollarSign } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 
 import s from "./Navigation.module.css";
+import clsx from "clsx";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
 
 const Navigation = () => {
   return (
     <nav className={s.nav}>
       <ul className={s.list}>
         <li className={s.item}>
-          <NavLink className={s.link} to="/home">
+          <NavLink className={buildLinkClass} to="/dashboard/home">
             <IoMdHome className={s.icon} />
             <p className={s.title}>Home</p>
           </NavLink>
         </li>
         <li className={s.item}>
-          <NavLink className={s.link} to="/statistics">
+          <NavLink className={buildLinkClass} to="/dashboard/statistics">
             <BiStats className={s.icon} />
             <p className={s.title}>Statistics</p>
           </NavLink>
         </li>
         <li className={s.item}>
-          <NavLink to="/currency">
+          <NavLink className={buildLinkClass} to="/dashboard/currency">
             <FaDollarSign className={s.icon} />
           </NavLink>
         </li>

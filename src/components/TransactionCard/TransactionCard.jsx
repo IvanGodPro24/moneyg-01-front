c3015b9034814618a337c6d91cd33a917303085f
Dmytro Koval <kdmytro793@gmail.com>
2025-05-03 00:02:52 +0300

    add-spiner-btm-delete

--- src/components/TransactionCard/TransactionCard.jsx
+++ src/components/TransactionCard/TransactionCard.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { LuPencil } from "react-icons/lu";
 import { format } from "date-fns";
+import { ClipLoader } from "react-spinners";
 
 import { deleteTransaction } from "../../redux/transactions/operations";
 import s from "./TransactionCard.module.css";
@@ -9,18 +10,21 @@ import TransactionEditForm from "../TransactionEditForm/TransactionEditForm";
 
 const TransactionCard = ({ id, date, category, comment, sum, type }) => {
   const dispatch = useDispatch();
-
   const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleToggleModal = () => {
     setIsOpen((prev) => !prev);
   };
 
   const handleDelete = async () => {
+    setLoading(true);
     try {
       await dispatch(deleteTransaction({ _id: id, type: sum })).unwrap();
     } catch (error) {
       console.log(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -47,9 +51,16 @@ const TransactionCard = ({ id, date, category, comment, sum, type }) => {
         {sum}
       </p>
       <div className={s.btn}>
-        <button className={s.delete} onClick={handleDelete}>
-          Delete
-        </button>
+        {loading ? (
+          <div className={s.loader}>
+            <ClipLoader size={25} color="#3498db" />
+          </div>
+        ) : (
+          <button className={s.delete} onClick={handleDelete}>
+            Delete
+          </button>
+        )}
+
         <button className={s.edit} onClick={handleToggleModal}>
           <LuPencil width="14" height="14" />
           <span className={s.text}>Edit</span>

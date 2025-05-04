b5a1b775d77336c1f1b15eb44ba1e358d1013022
Dmytro Koval <kdmytro793@gmail.com>
2025-05-04 23:31:43 +0300

    fix-num

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -6,13 +6,33 @@ import { selectTotalBalance } from "../../redux/auth/selectors";
 const Balance = () => {
   const totalBalance = useSelector(selectTotalBalance);
 
+  const formatNumber = (number) => {
+    if (number >= 1_000_000_000) {
+      return (
+        <>
+          {(number / 1_000_000_000).toFixed(1)}
+          <span className={s.textNum}>B</span>
+        </>
+      );
+    } else if (number >= 1_000_000) {
+      return (
+        <>
+          {(number / 1_000_000).toFixed(1)}
+          <span className={s.textNum}>M</span>
+        </>
+      );
+    } else {
+      return number.toFixed(2);
+    }
+  };
+
   return (
     <section className={s.section}>
       <div className={s.container}>
         <h2 className={s.title}>Your balance</h2>
         <p className={s.sum}>
           <span className={s.text}>₴</span>
-          {totalBalance?.toFixed(2)}
+          {formatNumber(totalBalance)}
         </p>
       </div>
     </section>

100d874b7f561fbca49ca48bfb6e16a6b2b2efb7
IvanGodPro24 <vanyanep@gmail.com>
2025-05-31 19:02:20 +0300

    Add sort

--- src/components/StatisticsTable/StatisticsTable.jsx
+++ src/components/StatisticsTable/StatisticsTable.jsx
@@ -1,18 +1,38 @@
+import { useEffect, useMemo, useState } from "react";
 import styles from "./StatisticsTable.module.css";
+import { TbArrowsSort } from "react-icons/tb";
 
 const StatisticsTable = ({ data, totalExpenses, totalIncome }) => {
-  const categories = data || [];
+  const categories = useMemo(() => data || [], [data]);
+
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [sortedCategories, setSortedCategories] = useState(categories);
+
+  const toggleFiltered = () => setIsFiltered((prev) => !prev);
+
+  useEffect(() => {
+    const sortedData = [...categories].sort((a, b) =>
+      isFiltered ? a.value - b.value : b.value - a.value
+    );
+    setSortedCategories(sortedData);
+  }, [isFiltered, categories]);
 
   return (
     <div className={styles.table}>
       <ul className={styles.list}>
         <li className={styles.listHeader}>
-          <div>
-            <span>Category</span>
+          <span>Category</span>
+          <div className={styles.container}>
             <span>Sum</span>
+            <button
+              onClick={toggleFiltered}
+              className={isFiltered ? "rotate-180" : "rotate-0"}
+            >
+              <TbArrowsSort />
+            </button>
           </div>
         </li>
-        {categories.map((item) => (
+        {sortedCategories.map((item) => (
           <li key={item.name} className={styles.item}>
             <div className={styles.label}>
               <span
